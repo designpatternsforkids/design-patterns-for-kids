a5fcd938c6ed7da77296f3550015ba3ed7eb72c6
Bunlong <bunlong.van@gmail.com>
2023-09-09 22:08:44 +0700

    SimpleFactory.java

--- examples/SimpleFactory.java
+++ examples/SimpleFactory.java
@@ -3,8 +3,32 @@
 // To execute
 // java SimpleFactory
 
+interface Laptop {
+  public int getScreenSize();
+}
+
+class MacLaptop implements Laptop {
+  protected int screenSize;
+
+  public MacLaptop(int screenSize) {
+    this.screenSize = screenSize;
+  }
+
+  public int getScreenSize() {
+    return this.screenSize;
+  }
+}
+
+class LaptopFactory {
+  public static Laptop makeLaptop(int screenSize) {
+    return new MacLaptop(screenSize);
+  }
+}
+
 class SimpleFactory {
   public static void main(String args[]) {
-    System.out.println("Hello world");
+    // Make me a 14 inch laptop
+    Laptop macLaptop = LaptopFactory.makeLaptop(14);
+    System.out.println(macLaptop.getScreenSize());
   }
 }
